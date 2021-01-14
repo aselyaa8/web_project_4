78877349cda7c137c67a97f88dfdff8017d388dc
Assel Kadyrkul <asel11k97@gmail.com>
2021-01-14 05:18:57 +0000

    First commit of project 4

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
 # Project 4: Around The U.S.
+# Author: Assel Kadyrkul 2020
 
 ### Overview
 
 * Figma
 * Images
+* JavaScript
+* Responsive Layout
 
 **Figma**
 
-* [Link to the project in Figma](https://www.figma.com/file/mUgu8OSHWE0M6p6vfwmdu9/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
+- Website's layout was made following guidlines in figma
+- [Link to the project in Figma](https://www.figma.com/file/mUgu8OSHWE0M6p6vfwmdu9/Sprint-4-Around-The-U.S.-desktop-mobile?node-id=0%3A1)
 
 **Images**
 
-The way you'll do this at work is by exporting images directly from Figma — we recommend doing that to practice more. Don't forget to optimize them [here](https://tinypng.com/), so your project loads faster. 
+- Images were optimized;
 
-Have fun with JavaScript!
+**JavaScript**
+
+JS was used to make buttons such as:
+
+- save(changes appear after saving edits)
+- like(for photos)
+- edit(user's name and description can be changed)
+- close(for closing popup window)
+
+
---
+++ script/script.js
@@ -0,0 +1,45 @@
+let edit = document.querySelector(".edit-button");
+let closeButton = document.querySelector(".close-button");
+let modalToggle = document.querySelector(".modal");
+let saveButton = document.querySelector(".save-button");
+let likeButton = document.querySelectorAll(".like-button");
+
+function openEdit() {
+  modalToggle.classList.add('modal_opened');
+  let profileName = document.querySelector(".profile__name");
+  let formName = document.querySelector(".form__name");
+  formName.value = profileName.textContent;
+  let profileInfo = document.querySelector(".profile__description");
+  let formInfo = document.querySelector(".form__description");
+  formInfo.value = profileInfo.textContent;
+}
+
+function closeEdit() {
+  modalToggle.classList.remove('modal_opened');
+}
+
+function saveForm() {
+  let profileName = document.querySelector(".profile__name");
+  let formName = document.querySelector(".form__name");
+  profileName.textContent = formName.value;
+  let profileInfo = document.querySelector(".profile__description");
+  let formInfo = document.querySelector(".form__description");
+  profileInfo.textContent = formInfo.value;
+  closeEdit();
+}
+
+function likeToggle(i) {
+  parseInt(i);
+  if (likeButton[i].classList.contains("like-button_disabled")) {
+    likeButton[i].classList.remove("like-button_disabled");
+    likeButton[i].classList.add("like-button_active");
+  } else {
+    likeButton[i].classList.remove("like-button_active");
+    likeButton[i].classList.add("like-button_disabled");
+  }
+}
+
+edit.addEventListener('click', openEdit);
+closeButton.addEventListener('click', closeEdit);
+saveButton.addEventListener('click', saveForm);
+
