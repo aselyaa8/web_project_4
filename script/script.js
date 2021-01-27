1a8725028aadd860e07aeb5cb5a5961144a3017f
Assel Kadyrkul <asel11k97@gmail.com>
2021-01-27 23:12:56 +0000

    add initial six cards

--- script/script.js
+++ script/script.js
@@ -1,43 +1,62 @@
-let edit = document.querySelector(".profile__edit-button");
-let closeButton = document.querySelector(".modal__close-button");
-let modalToggle = document.querySelector(".modal");
-let saveButton = document.querySelector(".form");
+
+
+// let edit = document.querySelector(".profile__edit-button");
+// let closeButton = document.querySelector(".modal__close-button");
+// let modalToggle = document.querySelector(".modal");
+// let saveButton = document.querySelector(".form");
 // let likeButton = document.querySelectorAll(".like-button");
-let profileName = document.querySelector(".profile__name");
-let formName = document.querySelector(".form__input_type_name");
-let profileInfo = document.querySelector(".profile__description");
-let formInfo = document.querySelector(".form__input_type_description");
+// let profileName = document.querySelector(".profile__name");
+// let formName = document.querySelector(".form__input_type_name");
+// let profileInfo = document.querySelector(".profile__description");
+// let formInfo = document.querySelector(".form__input_type_description");
+// let addButton = document.querySelector("profile__add-button");
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg"
+  }
+];
+
+initialCards.forEach((object)=>{
+  const cardTemplate = document.querySelector(".card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = object.link;
+  cardElement.querySelector(".card__text").textContent = object.name;
+
+  const cardsSection = document.querySelector(".cards");
+  cardsSection.prepend(cardElement);
+})
 
 
-function openEdit() {
-  modalToggle.classList.add('modal_opened');
-  formName.value = profileName.textContent;
-  formInfo.value = profileInfo.textContent;
+addButton.addEventListener("click", ()=> {
+  toggle();
+})
+
+addButton.addEventListener("click", toggle());
+
+
+function toggle(){
+
 }
-
-function closeEdit() {
-  modalToggle.classList.remove('modal_opened');
-}
-
-function saveForm(evt) {
-  evt.preventDefault();
-  profileName.textContent = formName.value;
-  profileInfo.textContent = formInfo.value;
-  closeEdit();
-}
-
-// function likeToggle(i) {
-//   parseInt(i);
-//   if (likeButton[i].classList.contains("card__like-button_disabled")) {
-//     likeButton[i].classList.remove("card__like-button_disabled");
-//     likeButton[i].classList.add("card__like-button_active");
-//   } else {
-//     likeButton[i].classList.remove("card__like-button_active");
-//     likeButton[i].classList.add("card__like-button_disabled");
-//   }
-// }
-
-edit.addEventListener('click', openEdit);
-closeButton.addEventListener('click', closeEdit);
-saveButton.addEventListener('submit', saveForm);
-
